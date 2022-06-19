9319406e51c3d2fb95cb8aaaea0c695067e88ba5
17043003 <ishizuka533725@gmail.com>
2022-06-20 04:41:34 +0900

    news livedata is changed, it is shown in textview.

--- app/src/main/java/com/ishzk/android/samplemvvmapp/MainActivity.kt
+++ app/src/main/java/com/ishzk/android/samplemvvmapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.ishzk.android.samplemvvmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import com.ishzk.android.samplemvvmapp.databinding.ActivityMainBinding
+import com.ishzk.android.samplemvvmapp.model.News
+import com.ishzk.android.samplemvvmapp.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+
+        val observer = Observer<News>{
+            val newsTextView: TextView = findViewById(R.id.newsText)
+            val newsContentTextView: TextView = findViewById(R.id.newsContentText)
+            newsTextView.text = it.title
+            newsContentTextView.text = it.content
+        }
+        viewModel.newsLiveData.observe(this, observer)
     }
 }

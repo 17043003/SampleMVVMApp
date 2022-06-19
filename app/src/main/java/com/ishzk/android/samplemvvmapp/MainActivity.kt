1913bcb4281aa807ec2dfbd7f83c4951cf16aaa4
17043003 <ishizuka533725@gmail.com>
2022-06-20 05:41:55 +0900

    show livedata on TextView using databinding.

--- app/src/main/java/com/ishzk/android/samplemvvmapp/MainActivity.kt
+++ app/src/main/java/com/ishzk/android/samplemvvmapp/MainActivity.kt
@@ -2,13 +2,9 @@ package com.ishzk.android.samplemvvmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import com.ishzk.android.samplemvvmapp.databinding.ActivityMainBinding
-import com.ishzk.android.samplemvvmapp.model.News
 import com.ishzk.android.samplemvvmapp.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -20,13 +16,6 @@ class MainActivity : AppCompatActivity() {
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = viewModel
-
-        val observer = Observer<News>{
-            val newsTextView: TextView = findViewById(R.id.newsText)
-            val newsContentTextView: TextView = findViewById(R.id.newsContentText)
-            newsTextView.text = it.title
-            newsContentTextView.text = it.content
-        }
-        viewModel.newsLiveData.observe(this, observer)
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/ishzk/android/samplemvvmapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ishzk/android/samplemvvmapp/viewmodel/MainViewModel.kt
@@ -3,11 +3,12 @@ package com.ishzk.android.samplemvvmapp.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ishzk.android.samplemvvmapp.model.News
+import java.util.*
 
-class MainViewModel(): ViewModel() {
+class MainViewModel: ViewModel() {
     val newsLiveData: MutableLiveData<News> by lazy { MutableLiveData() }
 
     fun fetchNews(){
-        newsLiveData.value = News(title = "sample title", content = "test content")
+        newsLiveData.value = News(title = "Title:${Date()}", content = "Content:${Date()}")
     }
 }

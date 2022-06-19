3d13ee8733682c63b0676716e6b20ca94f762d4c
17043003 <ishizuka533725@gmail.com>
2022-06-20 04:39:35 +0900

    fetched data is set livedata in viewmodel.

--- app/src/main/java/com/ishzk/android/samplemvvmapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ishzk/android/samplemvvmapp/viewmodel/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.ishzk.android.samplemvvmapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ishzk.android.samplemvvmapp.model.News
 
 class MainViewModel(): ViewModel() {
-    fun fetchNews(): News{
-        return News(title = "sample title", content = "test content")
+    val newsLiveData: MutableLiveData<News> by lazy { MutableLiveData() }
+
+    fun fetchNews(){
+        newsLiveData.value = News(title = "sample title", content = "test content")
     }
 }
